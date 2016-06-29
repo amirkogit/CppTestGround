658147fa6a38169f440f6dd5898852bdb300defd
amirkogit <amirkomail@gmail.com>
2016-06-29 10:53:15 -0400

    domain/c++/overload operator

--- hackerrank/domain_cpp_challenges.cpp
+++ hackerrank/domain_cpp_challenges.cpp
@@ -341,7 +341,73 @@ void IntroVariableSizedArrays()
     }
 
 }
+///////////////////////////////////////////////////////////
 
+
+///////////////////////////////////////////////////////////
+// Introduction/Overload operators
+class Complex
+{
+public:
+    int a, b;
+    void input(string s)
+    {
+        int v1 = 0;
+        int i = 0;
+        while (s[i] != '+')
+        {
+            v1 = v1 * 10 + s[i] - '0';
+            i++;
+        }
+        while (s[i] == ' ' || s[i] == '+' || s[i] == 'i')
+        {
+            i++;
+        }
+        int v2 = 0;
+        while (i<s.length())
+        {
+            v2 = v2 * 10 + s[i] - '0';
+            i++;
+        }
+        a = v1;
+        b = v2;
+    }
+};
+
+//Overload operators + and << for the class complex
+//+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
+//<< should print a complex number in the format "a+ib"
+Complex operator +(const Complex& lhs, const Complex& rhs)
+{
+    Complex result;
+    result.a = lhs.a + rhs.a;
+    result.b = lhs.b + rhs.b;
+    return result;
+}
+
+ostream& operator <<(ostream& out_stream, Complex c)
+{
+    out_stream << c.a << "+i" << c.b;
+    return out_stream;
+}
+
+void IntroOverloadOperator()
+{
+    Complex x, y;
+    string s1, s2;
+    cin >> s1;
+    cin >> s2;
+    x.input(s1);
+    y.input(s2);
+    Complex z = x + y;
+    cout << z << endl;
+}
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+// Main function for calling other sub functions
+// Uncomment the method name that you want to test
 void HackerRankChallenges::RunDomainCppChallenges()
 {
     std::cout << "Running all C++ domain challenges." << endl;
@@ -360,7 +426,9 @@ void HackerRankChallenges::RunDomainCppChallenges()
 
     //IntroOperatorOverloading();
 
-    IntroVariableSizedArrays();
+    //IntroVariableSizedArrays();
+
+    IntroOverloadOperator();
 }
 
 
