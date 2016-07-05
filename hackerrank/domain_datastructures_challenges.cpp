2c4926373911701918743550e9efa7b3a9f2e5e1
amirkogit <amirkomail@gmail.com>
2016-07-05 10:25:58 -0400

    domain/datastructures/dynamic_arrays

--- hackerrank/domain_datastructures_challenges.cpp
+++ hackerrank/domain_datastructures_challenges.cpp
@@ -37,10 +37,75 @@
 
 using namespace std;
 
+namespace DynamicArrays {
+    enum class QueryType {
+        Type_1 = 1,
+        Type_2
+    };
+
+    struct Query {
+        int type;
+        int x;
+        int y;
+    };
+
+    void DynamicArrays()
+    {
+        // read number of sequences
+        int no_of_sequences{ 0 };
+        cin >> no_of_sequences;
+
+        // read number of queries
+        int no_of_queries;
+        cin >> no_of_queries;
+
+        vector<Query> query_list;
+
+        // read all queries
+        for (int idx_query = 0; idx_query < no_of_queries; idx_query++) {
+            Query q;
+            cin >> q.type;
+            cin >> q.x;
+            cin >> q.y;
+            query_list.push_back(q);
+        }
+
+        // last answer variable
+        int last_answer{ 0 };
+
+        // create empty sequences
+        vector<vector<int>> seq_list(no_of_sequences);
+
+        // execute all queries from the query list
+        for (auto elem : query_list) {
+            QueryType type = static_cast<QueryType>(elem.type);
+
+            if (type == QueryType::Type_1) {
+                // find the sequence index
+                int seq_idx = (elem.x ^ last_answer) % no_of_sequences;
+
+                // append integer y to the sequence seq
+                seq_list[seq_idx].push_back(elem.y);
+            }
+            else if (type == QueryType::Type_2) {
+                // find the sequence index
+                int seq_idx = (elem.x ^ last_answer) % no_of_sequences;
+
+                last_answer = elem.y % (seq_list[seq_idx].size());
+
+                cout << seq_list[seq_idx][last_answer] << endl;
+
+                last_answer = seq_list[seq_idx][last_answer];
+            }
+        }
+    }
+} // end of namespace DynamicArrays
+
 void HackerRankChallenges::RunDomainDataStructuresChallenges()
 {
      std::cout << "Running all data structures domain challenges. " << endl;
-     
+
+     DynamicArrays::DynamicArrays();
 }
 
 
