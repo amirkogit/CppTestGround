85c90eeb7416e5a2c08edf86b3dd358c9d409738
amirkogit <amirkomail@gmail.com>
2016-10-13 14:21:24 -0400

    code clean up

--- src/common_utilities.h
+++ src/common_utilities.h
@@ -23,10 +23,6 @@
 #include <string>
 #include <iostream>
 
-#include <crtdbg.h>
-
-#define ASSERT _ASSERTE
-
 class CommonUtilities
 {
 public:
--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -18,14 +18,9 @@
 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include "common_utilities.h"
-
-static_assert(sizeof(float) == 4, "can't serialize floats !");
-
-
-
-#include <windows.h>
+#include <crtdbg.h>
 #include <stdio.h>
+#include <windows.h>
 
 #define ASSERT _ASSERTE
 
