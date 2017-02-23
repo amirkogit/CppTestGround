bf00ab68d1c638a6ac06c5d13c33bd1bea0a974f
amirkogit <amirkomail@gmail.com>
2017-02-23 14:07:00 -0500

    trying some basic thread

---
+++ sandbox/Sandbox/Threads/Source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include <thread>
+
+using namespace std;
+
+void do_something(int& i)
+{
+    ++i;
+    cout << "inside do_something\n";
+}
+
+struct func
+{
+    int& i;
+
+    func(int& i_) :i(i_) {}
+
+    void operator()()
+    {
+        for (unsigned j = 0; j<1000000; ++j)
+        {
+            do_something(i);
+        }
+    }
+};
+
+
+void oops()
+{
+    int some_local_state = 0;
+    func my_func(some_local_state);
+    std::thread my_thread(my_func);
+    my_thread.detach();
+    //my_thread.join();
+}
+
+int main()
+{
+    oops();
+
+    getchar();
+    return 0;
+}
