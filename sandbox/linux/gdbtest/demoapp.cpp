6036779cb573f6c7c5cf5263dd8c029708319de8
amirkogit <amirkomail@gmail.com>
2017-07-28 16:40:04 -0400

    updated with more examples.

--- sandbox/linux/gdbtest/demoapp.cpp
+++ sandbox/linux/gdbtest/demoapp.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <string>
 
 using namespace std;
@@ -8,6 +9,9 @@ int main()
 {
     cout << "gdb debugger demo..." << endl;
 
+    int size  = 10;
+    std::string name = "hello";
+
     vector<int> i_vec;
     i_vec.push_back(10);
     i_vec.push_back(20);
@@ -15,6 +19,8 @@ int main()
 
     cout << "Size of a vector: " << i_vec.size() << endl;
 
+    map<string,int> name_age_map = {{"Bob",30},{"Harry",45},{"Susan",26}};
+    cout << "Size of a map: " << name_age_map.size() << endl;
+    
     cout << "End of program" << endl;
-
 }
