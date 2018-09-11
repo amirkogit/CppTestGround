fc282f69ab729c3fff18bea1a97f59f22b7e9a2a
Amir Shrestha <amirkomail@gmail.com>
2018-09-11 14:31:56 -0400

    Simple node data structure that can hold any types.

--- cpp17Test/Cpp17Test/ds/node.h
+++ cpp17Test/Cpp17Test/ds/node.h
@@ -3,6 +3,8 @@
 #include <iostream>
 
 namespace CoreDS {
+
+// Simple node data structure that can hold only integer values
 class NodeInt final
 {
 public:
@@ -19,7 +21,34 @@ private:
     NodeInt* m_next = nullptr;
 };
 
-static void printNode(NodeInt* node)
+static void printNodeInt(NodeInt* node)
+{
+    using namespace std;
+
+    while(node != nullptr) {
+        cout << node->getValue() << " -> ";
+        node = node->getNext();
+    }
+    cout << "NULL" << endl;
+}
+
+// Generic node data structure that can hold any types
+template<typename T>
+class Node {
+public:
+    Node(T value) : m_value(value), m_next(nullptr) {}
+    ~Node() { }
+    T getValue() { return m_value; }
+    void setNext(Node<T>* node) { m_next = node; }
+    Node<T>* getNext() { return m_next; }
+
+private:
+    T m_value;
+    Node<T>* m_next = nullptr;
+};
+
+template<typename T>
+static void printNode(Node<T>* node)
 {
     using namespace std;
 
--- cpp17Test/Cpp17Test/node_tests.cpp
+++ cpp17Test/Cpp17Test/node_tests.cpp
@@ -17,10 +17,10 @@ TEST_CASE("Simple integer node test", "[Test1]")
     node1.setNext(&node2);
     node2.setNext(&node3);
 
-    CoreDS::printNode(&node1);
+    CoreDS::printNodeInt(&node1);
 }
 
-TEST_CASE("Simpe integer node test with pointers", "[Test1")
+TEST_CASE("Simpe integer node test with pointers", "[Test1]")
 {
     using namespace CoreDS;
     using namespace std;
@@ -33,6 +33,50 @@ TEST_CASE("Simpe integer node test with pointers", "[Test1")
     node2->setNext(node3.get());
     node3->setNext(node4.get());
 
-    printNode(node1.get());
+    printNodeInt(node1.get());
+}
 
+TEST_CASE("Generic Node Test - Int", "[Test1]")
+{
+    using namespace std;
+    using namespace CoreDS;
+
+    auto node1 = make_unique<Node<int>>(9);
+    auto node2 = make_unique<Node<int>>(12);
+    node1->setNext(node2.get());
+
+    cout << "Nodes containing integer nodes\n";
+    printNode(node1.get());
+}
+
+TEST_CASE("Generic Node Test - float", "[Test1]")
+{
+    using namespace std;
+    using namespace CoreDS;
+
+    auto node1 = make_unique<Node<float>>(53.80);
+    auto node2 = make_unique<Node<float>>(12.33);
+    auto node3 = make_unique<Node<float>>(-82.58);
+
+    node1->setNext(node2.get());
+    node2->setNext(node3.get());
+
+    cout << "Nodes containing float values: \n";
+    printNode(node1.get());
+}
+
+TEST_CASE("Generic Node Test - string", "[Test1]")
+{
+    using namespace std;
+    using namespace CoreDS;
+
+    auto node1 = make_unique<Node<string>>("this");
+    auto node2 = make_unique<Node<string>>("is");
+    auto node3 = make_unique<Node<string>>("C++17");
+
+    node1->setNext(node2.get());
+    node2->setNext(node3.get());
+
+    cout << "Nodes containing string values: \n";
+    printNode(node1.get());
 }
